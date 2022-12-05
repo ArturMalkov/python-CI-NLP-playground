1fb615b63b67ce293cb5f344f48195277d605c76
Artur <artur180892@mail.ru>
2022-12-05 10:17:46 +0300

    Add FastAPI functionality

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
+import uvicorn
+from fastapi import FastAPI
+
 from mylib import logic
 
 
-print(logic.wiki())
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Wikipedia API: use /search or /wiki"}
+
+
+@app.get("/search/{value}")
+def search(value: str):
+    """Page to search in Wikipedia"""
+    result = logic.search_wiki(value)
+    return {"result": result}
+
+
+@app.get("/wiki/{name}")
+def wiki(name: str):
+    """Retrieve Wikipedia page"""
+    result = logic.wiki(name)
+    return {"result": result}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
